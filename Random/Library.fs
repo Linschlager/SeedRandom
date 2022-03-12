9f7b50a1b0ace5f358932913e60e40da0218fbec
Linus Vettiger <linus4123@gmail.com>
2022-03-12 19:10:05 +0100

    Initial commit

---
+++ Random/Library.fs
@@ -0,0 +1,31 @@
+﻿namespace Random
+
+type Random(?seed: int) =
+    let lstate = obj
+    let mutable state : uint=
+        match seed with
+        | Some s -> uint s
+        | None -> (1u <<< 31) ||| 1u
+
+    member private x.NextBit () : uint =
+        lock lstate <| fun _ ->
+            let bit = state &&& 1u
+            let newFirst : uint = state ^^^ (state >>> 1) ^^^ (state >>> 2) ^^^ (state >>> 7) &&& 1u
+            let newState = (state >>> 1) ||| (newFirst <<< 31)
+            state <- newState
+            bit
+
+    member x.Next (min: int, max: int): int =
+        let diff = Util.toUInt32 max - Util.toUInt32 min
+        
+        let mutable bigRndNum = 0u
+        for i = 0 to 63 do
+            let bit = x.NextBit () 
+            bigRndNum <- bigRndNum <<< 1 ||| bit
+        bigRndNum % diff + Util.toUInt32 min
+        |> Util.fromUInt32
+
+    member x.Next (): int =
+        let min = 0
+        let max = System.Int32.MaxValue
+        x.Next(min, max)
---
+++ TestRandom/TestUtil.fs
@@ -0,0 +1,43 @@
+module Tests
+
+open Xunit
+open Random.SeedRandom
+
+[<Fact>]
+let ``Can generate a valid number in a range`` () =
+    let rnd = Random ()
+    let num = rnd.Next (10, 20)
+    Assert.InRange (num, 10, 20)
+
+[<Fact>]
+let ``Can generate a valid number in a range (negative)`` () =
+    let rnd = Random ()
+    let num = rnd.Next (-10, 0)
+    Assert.InRange (num, -10, 0)
+
+[<Fact>]
+let ``Can generate random numbers`` () =
+    let rnd = Random ()
+    let nums : int array = Array.init 100 <| fun _ -> rnd.Next ()
+    let differentNums =
+        Array.distinct nums
+        |> Array.length
+
+    Assert.True (differentNums > 95, $"Expected at least 95 different numbers. Got %i{differentNums} in %A{nums}")
+
+[<Fact>]
+let ``Can generate the same numbers given the same seed`` ()  =
+    let rnd1 = Random 42
+    let nums1 : int array = Array.init 100 <| fun _ -> rnd1.Next ()
+    let rnd2 = Random 42
+    let nums2 : int array = Array.init 100 <| fun _ -> rnd2.Next ()
+    Assert.Equal<int array> (nums1, nums2)
+
+[<Fact>]
+let ``Can generate any number given the correct seed`` ()  =
+    let rnd1 = Random ()
+    let set = System.Collections.Generic.HashSet<int> ()
+
+    for i = 1 to System.Int32.MaxValue do
+        let num = rnd1.Next()
+        Assert.True (set.Add num, $"Failed at index %i{i}")
